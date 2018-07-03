3fad7fcf379edeaa56c1b25cb5ebfc6c34a1be0f
Alistair Buxton <a.j.buxton@gmail.com>
2018-07-03 03:54:13 +0100

    Make Protector raise AttributeError instead of SyntaxError.
    It makes more sense as we are blocking operations on Attributes.

--- pydo/protector.py
+++ pydo/protector.py
@@ -9,9 +9,9 @@ class Protector(object):
     def __getitem__(self, k):
         if k in self._protected:
             if callable(self._dict[k]):
-                raise SyntaxError("'{:s}' is a module instance method. Access it with 'pydo.this_module().{:s}'".format(k, k))
+                raise AttributeError("'{:s}' is a module instance method. Access it with 'pydo.this_module().{:s}'".format(k, k))
             else:
-                raise SyntaxError("'{:s}' is a module instance attribute. Access it with 'pydo.this_module().{:s}'".format(k, k))
+                raise AttributeError("'{:s}' is a module instance attribute. Access it with 'pydo.this_module().{:s}'".format(k, k))
         return self._dict[k]
 
     def __setitem__(self, k, v):
@@ -19,12 +19,12 @@ class Protector(object):
             if v is SyntaxCheckerIgnore:
                 return
             if callable(self._dict[k]):
-                raise SyntaxError("'{:s}' is a module instance method. Define it with the '@command' decorator.".format(k, k))
+                raise AttributeError("'{:s}' is a module instance method. Define it with the '@command' decorator.".format(k, k))
             else:
-                raise SyntaxError("'{:s}' is a module instance attribute. Access it with 'pydo.this_module().{:s}'".format(k, k))
+                raise AttributeError("'{:s}' is a module instance attribute. Access it with 'pydo.this_module().{:s}'".format(k, k))
         self._dict[k] = v
 
     def __delitem__(self, k):
         if k in self._protected:
-            raise SyntaxError("'{:s}' is a module instance attribute and cannot be deleted.".format(k))
+            raise AttributeError("'{:s}' is a module instance attribute and cannot be deleted.".format(k))
         del self._dict[k]
