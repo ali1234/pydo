b94d837b9b8e4c7b38ad60d004070b691058c2f7
Alistair Buxton <a.j.buxton@gmail.com>
2018-07-04 02:17:14 +0100

    Complete setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ setup(
     name='pydo',
     version='',
     packages=['pydo'],
-    url='',
-    license='',
+    url='https://github.com/ali1234/pydo',
+    license='GPLv3',
     author='Alistair Buxton',
-    author_email='',
-    description=''
+    author_email='a.j.buxton@gmail.com',
+    description='A metabuild automation system.',
+    install_requires=['descriptors', 'sh'],
+    entry_points={
+        'console_scripts': [
+            'pydo = pydo.__main__:main'
+        ]
+    },
 )
