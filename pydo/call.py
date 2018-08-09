c761651bd26b6046a236d868cad958650405976d
Alistair Buxton <a.j.buxton@gmail.com>
2018-08-09 10:28:24 +0100

    Environment passing

--- pydo/call.py
+++ pydo/call.py
@@ -5,9 +5,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def call(commands, check=True, shell=False):
+def call(commands, check=True, shell=False, env=None):
     for c in commands:
         logger.info(c)
         if not shell:
             c = shlex.split(c)
-        subprocess.run(c, check=check, shell=shell)
+        subprocess.run(c, check=check, shell=shell, env=env)
